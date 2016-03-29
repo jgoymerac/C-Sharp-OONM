27f8696349b05a246b3ecd546b8b54011fa57be8
jgoymerac <jgoymerac@gmail.com>
2016-03-29 17:35:12 -0500

    started CylnCord CLass

---
+++ C-Sharp-Hmwk2/C-Sharp-Hmwk2/CylnCord.cs
@@ -0,0 +1,12 @@
+﻿namespace C_Sharp_Hmwk2
+{
+    internal class CylnCord<T>
+    {
+        private T m_r;
+        private T m_theta;
+        private T m_z;
+        private void cylnCopy(ref CylnCord<T> cc) { m_r = cc.m_r;m_theta = cc.m_theta;m_z = cc.m_z;return; }
+        
+
+    }
+}
