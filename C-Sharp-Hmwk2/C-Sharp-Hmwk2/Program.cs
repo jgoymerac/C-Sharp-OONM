2d5bce88034a915de3ab531265c555e06e59b0af
jgoymerac <jgoymerac@gmail.com>
2016-03-29 15:40:21 -0500

    init

---
+++ C-Sharp-Hmwk2/C-Sharp-Hmwk2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace C_Sharp_Hmwk2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            float num;
+            Norm1<float> norm;
+
+
+
+
+            Console.WriteLine("Enter the file name:");
+            string filename = Console.ReadLine();
+            try
+            {
+                using( StreamReader sr = new StreamReader(filename))
+                {
+
+                }
+            }
+
+        }
+    }
+}
